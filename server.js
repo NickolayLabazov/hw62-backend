cce811834b5f1a860c948b9dad8b5d8ee3d7083f
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-11-06 13:12:44 +0300

    6.11

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+const notes = [];
+
+const router = new Router();
+
+router.get('/notes', async (ctx, next) => {
+    ctx.response.body = notes;    
+});
+
+router.post('/notes', async(ctx, next) => {   
+    let note = JSON.parse(ctx.request.body)
+    notes.push(note);
+    ctx.response.status = 204;    
+});
+
+router.delete('/notes/:id', async(ctx, next) => {
+    
+    const noteId = String(ctx.params.id);    
+    const index = notes.findIndex(o => o.id === noteId);
+    if (index !== -1) {
+        notes.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
